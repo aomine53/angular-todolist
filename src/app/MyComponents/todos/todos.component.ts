485f1028889dea53733d0ea1baf1081290dd49df
Digvijay Shinde <digvijay_shinde1@persistent.com>
2022-08-29 19:19:58 +0530

    first commit

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from '../../Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos:Todo[];
+  constructor() { 
+    this.todos=[
+      {
+        sno: 1,
+        title: "This is title",
+        desc: "This is description",
+        active: true
+      }, 
+      {
+        sno: 2,
+        title: "This is title",
+        desc: "This is description",
+        active: true
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+  deleteTodo(todo : Todo){
+    const index = this.todos.indexOf(todo);    
+    this.todos.splice(index, 1);
+    console.log("Todo deleted!");
+    
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
